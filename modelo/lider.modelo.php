85acca0fc4fefaaa4d0341e1ce4c76ec69d201f9
guido <ruizbenitezguido11@gmail.com>
2022-11-30 08:02:27 -0300

    actualizado

--- modelo/lider.modelo.php
+++ modelo/lider.modelo.php
@@ -174,13 +174,10 @@ class ModeloLider{
 	
 		if($stmt -> execute()){
 
-			$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id_persona = :id");
 
-			$stmt -> bindParam(":id", $datos["id_persona"], PDO::PARAM_INT);
-
-			if($stmt -> execute()){
-				return "ok";
-			}
+			
+			return "ok";
+			
 			
 		
 		}else{
